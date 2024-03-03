c930aed7766519151d0a923d5e54235439822b19
Atlas <nicole.scalera1@marist.edu>
2024-03-03 15:03:05 -0500

    Update again
    - Added 'express' and 'nodemon' dependencies.
    - Created index.html file, which is the homepage.
    - Filled in app.js with 'server2.js' from Lab 3 (see GitHub).

--- server/app.js
+++ server/app.js
@@ -1,4 +1,15 @@
-// entry point file
+const express = require('express'); // need to install exp dependency (require indicates a dependency)
+const app = express();
 
-console.log("test");
-console.log("test2");
+// the '/' goes to the homepage of the app
+app.get(['/', '/index.html'], function (req, res) {
+    // This is what you get back: a file named 'index.html' that displays the homepage
+    res.sendFile('index.html', { root: './client/views' })
+    // res is the "response," what the client actually gets
+})
+
+app.get('/page1.html', function (req, res) {
+    res.sendFile('page1.html', { root: './' })
+})
+
+app.listen(1337, () => console.log('Marist Chatter listening on port 1337!'));
