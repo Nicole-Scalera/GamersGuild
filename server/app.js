ad5c42f08ee4ee73a047a8b6d2f99cf52f10d2ce
Atlas <nicole.scalera1@marist.edu>
2024-03-03 15:24:54 -0500

    More changes
    - Added that PORT.
    - Edited terminal output statement ("Marist Chatter") to show what port we're on (in other words, it's not hardcoded in).

--- server/app.js
+++ server/app.js
@@ -1,5 +1,12 @@
-const express = require('express'); // need to install exp dependency (require indicates a dependency)
-const app = express();
+// http://localhost:1337
+// ^ CLICK ME
+
+const PORT = process.env.PORT || 1337;
+             // if this process.env port is
+             // not defined, then 1337 is chosen.
+
+const express = require('express'); // Express is a dependency that needs
+const app = express();              // to be installed through the terminal.
 
 // the '/' goes to the homepage of the app
 app.get(['/', '/index.html'], function (req, res) {
@@ -12,4 +19,11 @@ app.get('/page1.html', function (req, res) {
     res.sendFile('page1.html', { root: './' })
 })
 
-app.listen(1337, () => console.log('Marist Chatter listening on port 1337!'));
+// app.listen is basically the notification bell.
+// E.g. someone's extension number on a phone number.
+app.listen(PORT, () => console.log('Marist Chatter listening on this port ' + PORT + '!')); // NOT hardcoded.
+// console.log gives info to the user
+// We need to make a variable for this port.
+// You can make '1337' pretty much anything you want.
+
+
