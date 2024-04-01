e9964b91204ec7ed31501e2cd5d8ea3352cae962
Nicole Scalera <85967480+Nicole-Scalera@users.noreply.github.com>
2024-04-01 11:58:26 -0400

    Other changes

---
+++ server/controller/userController.js
@@ -0,0 +1,13 @@
+// The controller is supposed to handle the functions that come in.
+
+const createNewUser = async (request,response) => {
+    // Takes in the request, and sends out a response
+    // 
+    // Parse the user's password.
+    // Save the user to database.
+    // send a reponse out from the function
+    // (could be something like success:true).
+
+    // The views folder handles sending down HTML
+
+}
---
+++ server/model/user.js
@@ -0,0 +1,9 @@
+// This will define the shape of a user that gets stored in the database.
+
+// When we talk about shape, we're talking about an object.
+
+const UserModel = {
+    // Model is a representation of the data
+    username: "user's username",
+    password: "user's password"
+}
