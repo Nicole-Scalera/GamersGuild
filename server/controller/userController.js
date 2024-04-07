f47482072634a22cc9420a77d31cf2590dd443ae
Nicole Scalera <85967480+Nicole-Scalera@users.noreply.github.com>
2024-04-07 14:15:49 -0400

    New files and code added
    - Created a few more files that were listed in the screenshot attached to Lab 5.
    - Also added code in userController.js detailed under 2.2 Controller creation on the same document.

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,13 +1,49 @@
 // The controller is supposed to handle the functions that come in.
 
-const createNewUser = async (request,response) => {
-    // Takes in the request, and sends out a response
-    // 
-    // Parse the user's password.
-    // Save the user to database.
-    // send a reponse out from the function
-    // (could be something like success:true).
+// const createNewUser = async (request,response) => {
+//     // Takes in the request, and sends out a response
+//     // Parse the user's password.
+//     // Save the user to database.
+//     // send a reponse out from the function
+//     // (could be something like success:true).
 
-    // The views folder handles sending down HTML
+//     // The views folder handles sending down HTML
 
+// }
+
+// ------------------------------------------
+// Create an empty array that will contain the
+// user model instances that are created.
+let users = [];
+
+// Create a user
+let peter = User.createUser("Peter", "Parker")
+
+// add the user to the array
+users.push(peter);
+// ------------------------------------------
+
+
+// ------------------------------------------
+// Send entire users array as the body of the
+// response as json.
+exports.getAllUsers = (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(users);
 }
+
+// Retrieve the user in the :index parameter
+// of the request and return as json.
+exports.getUser = (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(users[req.params.index]);
+}
+
+// Save a user
+exports.saveUser = (req, res) => {
+    let newUser = User.createUser(req.body.firstName, req.body.lastName);
+    users.push(newUser);
+    res.setHeader('Content-Type', 'application/json');
+    res.send(users);
+}
+// ------------------------------------------
